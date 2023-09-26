476d53ae05f5c6210a45577e0541369eb4504e36
printfCAT <cliffomanyo@gmail.com>
2023-09-27 00:26:04 +0300

    pycodestyle fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """ prints a Pascal's triangle of size n """
     if n <= 0:
         return []
 
@@ -7,7 +8,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
 
