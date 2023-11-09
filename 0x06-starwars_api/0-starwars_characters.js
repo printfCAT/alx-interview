54406b28fba596da10344bb32fec1c1d16f4469b
printfCAT <cliffomanyo@gmail.com>
2023-11-10 00:01:25 +0300

    semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,22 @@
 /* eslint-disable linebreak-style */
 const request = require('request');
 
-function getCharacters(movieId) {
-  return new Promise((resolve, reject) => {
-    request({ uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`, json: true },
-      (filmError, filmResponse, filmBody) => {
-        if (filmError) {
-          reject(new Error(`Error fetching film data: ${filmError.message}`));
-        } else {
-          const { characters } = filmBody;
-          const characterPromises = characters.map(
-            (characterUrl) => new Promise((characterResolve, characterReject) => {
-              request({ uri: characterUrl, json: true },
-                (characterError, characterResponse, characterBody) => {
-                  if (characterError) {
-                    characterReject(new Error(`Error fetching character data: ${characterError.message}`));
-                  } else {
-                    characterResolve(characterBody.name);
-                  }
-                });
-            }),
-          );
-
-          Promise.all(characterPromises)
-            .then((characterNames) => resolve(characterNames))
-            .catch((characterError) => reject(characterError));
-        }
-      });
-  });
-}
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node script.js <movie_id>');
-  process.exit(1);
-}
-
 const movieId = process.argv[2];
 
-getCharacters(movieId)
-  .then((characterNames) => {
-    characterNames.forEach((name) => console.log(name));
-  })
-  .catch((error) => console.error(`Error: ${error.message}`));
+const options = {
+  uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
+  json: true
+};
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const { characters } = body;
+
+  for (const character of characters) {
+    console.log(character.name);
+  }
+});
