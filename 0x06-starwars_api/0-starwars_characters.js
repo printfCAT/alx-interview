f7a0e2716bea9ba923f9419f754546ec20dd4858
printfCAT <cliffomanyo@gmail.com>
2023-11-10 00:11:51 +0300

    1st line fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,40 @@
-/* eslint-disable linebreak-style */
+#!/usr/bin/node
 const request = require('request');
+
+function getCharacters(movieId) {
+  return new Promise((resolve, reject) => {
+    request({ uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`, json: true }, (filmError, filmResponse, filmBody) => {
+      if (filmError) {
+        reject(new Error(`Error fetching film data: ${filmError.message}`));
+      } else {
+        const { characters } = filmBody;
+        const characterPromises = characters.map((characterUrl) => new Promise((characterResolve, characterReject) => {
+          request({ uri: characterUrl, json: true }, (characterError, characterResponse, characterBody) => {
+            if (characterError) {
+              characterReject(new Error(`Error fetching character data: ${characterError.message}`));
+            } else {
+              characterResolve(characterBody.name);
+            }
+          });
+        }));
+
+        Promise.all(characterPromises)
+          .then((characterNames) => resolve(characterNames))
+          .catch((characterError) => reject(characterError));
+      }
+    });
+  });
+}
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node script.js <movie_id>');
+  process.exit(1);
+}
 
 const movieId = process.argv[2];
 
-const options = {
-  uri: `https://swapi-api.alx-tools.com/api/films/${movieId}`,
-  json: true
-};
-
-request(options, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const { characters } = body;
-
-  for (const character of characters) {
-    console.log(character.name);
-  }
-});
+getCharacters(movieId)
+  .then((characterNames) => {
+    characterNames.forEach((name) => console.log(name));
+  })
+  .catch((error) => console.error(`Error: ${error.message}`));
