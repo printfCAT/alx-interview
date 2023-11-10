a980003690687ee1308426e5b36691b003a13af1
printfCAT <cliffomanyo@gmail.com>
2023-11-10 10:40:19 +0300

    task 0 fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,49 @@
 #!/usr/bin/python3
-""" define a function """
 
+"""
+A script that reads stdin line by line and computes stats.
+"""
 
 import sys
-import re
-import signal
 
-# Define a dictionary to store status code counts
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+if __name__ == "__main__":
+    st_code = {"200": 0,
+               "301": 0,
+               "400": 0,
+               "401": 0,
+               "403": 0,
+               "404": 0,
+               "405": 0,
+               "500": 0}
 
-# Variable to store the total file size
-total_file_size = 0
+    count = 1
+    file_size = 0
 
-# Variable to keep track of the line count
-line_count = 0
+    def parse_line(line):
+        """ Read, parse and get data """
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in st_code.keys():
+                st_code[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
 
-# Function to print the metrics
-def print_metrics():
-    """ function to print the metrics """
-    print("Total file size:", total_file_size)
-    for status_code, count in sorted(status_code_counts.items()):
-        if count > 0:
-            print(f"{status_code}: {count}")
+    def print_stats():
+        """ Print status in ascending order """
+        print("File size: {}".format(file_size))
+        for key in sorted(st_code.keys()):
+            if st_code[key]:
+                print("{}: {}".format(key, st_code[key]))
 
-# Signal handler for CTRL+C
-def signal_handler(sig, frame):
-    """ signal handler for CTRL+C """
-    print_metrics()
-    sys.exit(0)
-
-# Register the signal handler for CTRL+C
-signal.signal(signal.SIGINT, signal_handler)
-
-# Regular expression to match the input format
-pattern = re.compile(r'^(\d+\.\d+\.\d+\.\d+) - \[.*\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)')
-
-try:
-    for line in sys.stdin:
-        # Attempt to match the line with the regular expression
-        match = pattern.match(line)
-        if match:
-            ip_address, status_code, file_size = match.groups()
-            status_code = int(status_code)
-            file_size = int(file_size)
-            total_file_size += file_size
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
-            line_count += 1
-            if line_count % 10 == 0:
-                print_metrics()
-
-except KeyboardInterrupt:
-    print_metrics()
+    try:
+        for line in sys.stdin:
+            file_size += parse_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
